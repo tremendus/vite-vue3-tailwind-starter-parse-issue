8407aad4b06d158b9c18b63d6064383a76b7f5ac
Darren Carter <darren@tremendusapps.com>
2021-05-13 08:48:03 -0700

    initialize

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
+<script>
+import 'parse'
+</script>
+
 <template>
   <div>
     <header class="bg-white shadow" v-if="$route.meta.title">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">{{ $route.meta.title }}</h1>
       </div>
     </header>
